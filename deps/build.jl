530f537e9fb43d970e9e18170675ff81a9d44079
Marios Daoutis <mds@monads.io>
2015-02-14 00:15:12 +0100

    fixing bindpes build script

--- deps/build.jl
+++ deps/build.jl
@@ -11,7 +11,7 @@ extensions = ["", ".a", ".so.5", ".dylib"]
 aliases = vec(libnames.*transpose(suffixes).*reshape(options,(1,1,length(options))).*reshape(extensions,(1,1,1,length(extensions))))
 
 deps = [
-  ncurses = library_dependency("ncurses", aliases = aliases)
+ncurses = library_dependency("ncurses", aliases = aliases)
 ]
 
 provides(Sources, {
@@ -21,88 +21,133 @@ URI("http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz") => ncurses
 ncurses_home = get(ENV, "NCURSES_HOME", "") # If MAGICK_HOME is defined, add to library search path
 
 if !isempty(ncurses_home)
-    push!(DL_LOAD_PATH, ncurses_home)
-    push!(DL_LOAD_PATH, joinpath(ncurses_home,"lib"))
+  push!(DL_LOAD_PATH, ncurses_home)
+  push!(DL_LOAD_PATH, joinpath(ncurses_home,"lib"))
 end
 
 @linux_only begin
-    provides(AptGet, "ncurses", ncurses)
-    provides(AptGet, "ncurses", ncurses)
-    provides(Pacman, "ncurses", ncurses)
-    provides(Yum, "ncurses", ncurses)
+  provides(AptGet, "ncurses", ncurses)
+  provides(AptGet, "ncurses", ncurses)
+  provides(Pacman, "ncurses", ncurses)
+  provides(Yum, "ncurses", ncurses)
 end
 
 @windows_only begin
-    const OS_ARCH = (WORD_SIZE == 64) ? "x64" : "x86"
+  # TODO: remove me when upstream is fixed
+  error("Not supported")
+  # const OS_ARCH = (WORD_SIZE == 64) ? "x64" : "x86"
 end
+
+# @osx_only begin
+  # if Pkg.installed("Homebrew") === nothing
+  #         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+  # end
+  # using Homebrew
+  # provides( Homebrew.HB, "imagemagick", libwand, os = :Darwin, onload =
+  # """
+  # function __init__()
+  #     ENV["MAGICK_CONFIGURE_PATH"] = joinpath("$(Homebrew.prefix("imagemagick"))","lib","ImageMagick","config-Q16")
+  #     ENV["MAGICK_CODER_MODULE_PATH"] = joinpath("$(Homebrew.prefix("imagemagick"))", "lib","ImageMagick","modules-Q16","coders")
+  #     ENV["PATH"] *= ":" * joinpath("$(Homebrew.prefix("imagemagick"))", "bin")
+  # end
+  # """ )
+# end
 
 @osx_only begin
-    # if Pkg.installed("Homebrew") === nothing
-    #         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
-    # end
-    # using Homebrew
-    # provides( Homebrew.HB, "imagemagick", libwand, os = :Darwin, onload =
-    # """
-    # function __init__()
-    #     ENV["MAGICK_CONFIGURE_PATH"] = joinpath("$(Homebrew.prefix("imagemagick"))","lib","ImageMagick","config-Q16")
-    #     ENV["MAGICK_CODER_MODULE_PATH"] = joinpath("$(Homebrew.prefix("imagemagick"))", "lib","ImageMagick","modules-Q16","coders")
-    #     ENV["PATH"] *= ":" * joinpath("$(Homebrew.prefix("imagemagick"))", "bin")
-    # end
-    # """ )
-end
 
-const ncurses_install_name = "ncurses-5.9"
-srcdir = joinpath(BinDeps.depsdir(ncurses), ncurses_install_name)
+ncurses_install_name = "ncurses-5.9"
 prefix = BinDeps.depsdir(ncurses)
-const patch_file_src = "https://trac.macports.org/export/103963/trunk/dports/devel/ncurses/files/constructor_types.diff"
+srcdir = joinpath(prefix, "src", ncurses_install_name)
+patch_file_src = "https://trac.macports.org/export/103963/trunk/dports/devel/ncurses/files/constructor_types.diff"
+# patch_path = joinpath(srcdir, "c++")
+# patch_file = joinpath(patch_path, "ncurses-5.9.patch")
 
+provides(SimpleBuild,
+  (@build_steps begin
+    GetSources(ncurses)
+    @build_steps begin
+      ChangeDirectory(srcdir)
+      @build_steps begin
+        run(download_cmd(patch_file_src, joinpath(srcdir, "ncurses-5.9.patch")))
+        if success(`patch -p0 -N --dry-run --silent < ncurses-5.9.patch 2>/dev/null`)
+          `patch -p0 cursesf.h ncurses-5.9.patch`
+        end
+      end
+      @build_steps begin
+        ChangeDirectory(srcdir)
+            `./configure --prefix=$prefix --enable-dependency-linking --enable-pc-files --enable-sigwinch --enable-symlinks --enable-widec --with-manpage-format=normal --with-shared --enable-ext-colors --enable-ext-mouse --enable-getcap --enable-hard-tabs --enable-interop --enable-reentrant --with-pthread --enable-symlinks --enable-termcap --with-sysmouse --with-tlib=ncurses`
+          `make`
+          `make install`
+      end
+    end
+  end), ncurses, os = :Darwin)
+end
+# @build_steps begin
+  # ChangeDirectory(patch_path)
+  # @build_steps begin
+    #
+    # `patch -p1 --dry-run` < `ncurses-5.9.patch`
+  # end
+# end
+# @osx_only begin
+#   patch_path = joinpath(srcdir, "c++")
+#   ChangeDirectory(patch_path)
+#   run(download_cmd(patch_file_src, joinpath(patch_path, "ncurses-5.9.patch")))
+#   ChangeDirectory(joinpath(srcdir, "c++"))
+#   run(`patch -p1 --dry-run` < `ncurses-5.9.patch`)
+# end
 
-provides(BuildProcess,
-        (@build_steps begin
-          GetSources(ncurses)
-          CreateDirectory(srcdir)
-          @build_steps begin
-              ChangeDirectory(srcdir)
-              @osx_only begin
-                patch_path = joinpath(srcdir, "c++")
-                ChangeDirectory(patch_path)
-                run(download_cmd(patch_file_src, joinpath(patch_path, "ncurses-5.9.patch")))
-                ChangeDirectory(joinpath(srcdir, "c++"))
-                run(`patch -p1 --dry-run` < `ncurses-5.9.patch`)
-              end
-              `./configure --prefix=$prefix --enable-dependency-linking --enable-pc-files --enable-sigwinch --enable-symlinks --enable-widec --with-manpage-format=normal --with-shared --enable-ext-colors --enable-ext-mouse --enable-getcap --enable-hard-tabs --enable-interop --enable-reentrant --with-pthread --enable-symlinks --enable-termcap --with-sysmouse --with-tlib=ncurses`
-            `make`
-            `make install`
-            end
-        end), ncurses)
+# GetSources(ncurses)
+#
+# ChangeDirectory(srcdir)
+#     `./configure --prefix=$prefix --enable-dependency-linking --enable-pc-files --enable-sigwinch --enable-symlinks --enable-widec --with-manpage-format=normal --with-shared --enable-ext-colors --enable-ext-mouse --enable-getcap --enable-hard-tabs --enable-interop --enable-reentrant --with-pthread --enable-symlinks --enable-termcap --with-sysmouse --with-tlib=ncurses`
+#   `make`
+#   `make install`
+# @osx_only begin
+#   patch_path = joinpath(srcdir, "c++")
+#   ChangeDirectory(patch_path)
+#   run(download_cmd(patch_file_src, joinpath(patch_path, "ncurses-5.9.patch")))
+#   ChangeDirectory(joinpath(srcdir, "c++"))
+#   run(`patch -p1 --dry-run` < `ncurses-5.9.patch`)
+# end
+#
+# provides(SimpleBuild,
+#     (@build_steps begin
+#         GetSources(zlib)
+#         @build_steps begin
+#             ChangeDirectory(joinpath(BinDeps.depsdir(zlib),"src","zlib-1.2.7"))
+#             MakeTargets(["-fwin32/Makefile.gcc"])
+#             #MakeTargets(["-fwin32/Makefile.gcc","DESTDIR=../../usr/","INCLUDE_PATH=include","LIBRARY_PATH=lib","SHARED_MODE=1","install"])
+#         end
+#     end),zlib, os = :Windows)
 
 @BinDeps.install Dict([(:ncurses => :ncurses)])
 
 module CheckVersion
 include("deps.jl")
 if isdefined(:__init__)
-    __init__()
+  __init__()
 end
 nc_ver = ccall((:curses_version, ncurses), Ptr{UInt8}, ())
 if nc_ver == C_NULL
-    ver_str = split(bytestring(nc_ver), ' ')
-    ver_str_name = ver_str[1]
-    ver_str_num  = ver_str[2]
-    ver_num = split(ver_str_num, '.')
-    major = ver_num[1]
-    minor = ver_num[2]
-    build = ver_num[3]
-    vstr = string("v\"", join([major,minor,build], '.'), "\"")
-    open(joinpath(dirname(@__FILE__),"versioninfo.jl"), "w") do file
-        write(file, "const libversion = $vstr\n")
-    end
-    NCURSES_VERSION = vstr
-    println("NCurses Library Found, Version [$(vstr)")
+  ver_str = split(bytestring(nc_ver), ' ')
+  ver_str_name = ver_str[1]
+  ver_str_num  = ver_str[2]
+  ver_num = split(ver_str_num, '.')
+  major = ver_num[1]
+  minor = ver_num[2]
+  build = ver_num[3]
+  vstr = string("v\"", join([major,minor,build], '.'), "\"")
+  open(joinpath(dirname(@__FILE__),"versioninfo.jl"), "w") do file
+  write(file, "const libversion = $vstr\n")
+end
+NCURSES_VERSION = vstr
+println("NCurses Library Found, Version [$(vstr)")
 end
 
 # vstr = string("v\"", join(split(bytestring(p), ',')[1:3], '.'), "\"")
 # open(joinpath(dirname(@__FILE__),"versioninfo.jl"), "w") do file
-    # write(file, "const libversion = $vstr\n")
+# write(file, "const libversion = $vstr\n")
 # end
 end
 
